0aa56abc57044b57e686daaa4edeacf208fc1a9b
J <jr-wong@qq.com>
2018-05-29 14:46:41 +0800

    11

--- static.sh
+++ static.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+port=7081
+
+pid=$(ss -lp|grep $port|sed "s/.*pid=//g"|sed "s/,.*//g")
+if [[ ${#pid} != 0 ]];then
+    kill $pid
+fi
+
 git pull
 hexo g
-nohup http-server -p 7081 &
+nohup http-server -p $port &
